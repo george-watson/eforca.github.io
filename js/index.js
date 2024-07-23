1a97b6f40dcda29e201869161458a8c06a9608c5
Eliot Forcadell <eliot.forcadell@gmail.com>
2024-07-23 19:20:27 +0200

    Index seems to work

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $("#answer").on("change keyup paste", function () {
+        if ($(this).val().length === 4) {
+            if ($(this).val() === "1989") {
+                $('#info').removeClass("hidden");
+            }
+        }
+    })
+
+
+});
