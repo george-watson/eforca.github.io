d21498d2f7facbab55b47a883cc36ad0b76c2d74
Eliot Forcadell <eliot.forcadell@gmail.com>
2024-07-23 18:30:50 +0200

    Scene 2 seems to work

---
+++ js/scene2.js
@@ -0,0 +1,52 @@
+
+
+$(document).ready(function () {
+
+    var sequence = ['debut', 'fearless', 'speaknow', 'red', 'c1989', 'reputation',
+        'lover', 'folklore', 'evermore', 'midnights', 'ttpd'
+    ];
+    var humanSequence = [];
+
+    var nope = $("#nope");
+    var yay = $("#yay");
+    var erasButtons = $('.era-btn');
+
+
+    $.fn.randomize = function (selector) {
+        (selector ? this.find(selector) : this).parent().each(function () {
+            $(this).children(selector).sort(function () {
+                return Math.random() - 0.5;
+            }).detach().appendTo(this);
+        });
+
+        return this;
+    };
+
+    erasButtons.randomize();
+
+
+    function resetGame() {
+        humanSequence = [];
+    }
+
+    function handleClick(btn) {
+        const index = humanSequence.push(btn) - 1;
+
+        if (humanSequence[index] !== sequence[index]) {
+            nope.removeClass('hidden');
+            humanSequence = [];
+
+        } else {
+            if (humanSequence.length === sequence.length) {
+                yay.removeClass('hidden');
+            }
+        }
+    }
+
+    erasButtons.on('click', function () {
+        nope.addClass('hidden');
+        yay.addClass('hidden');
+        handleClick($(this).attr('id'));
+    });
+
+});
